f141c5bfbff8823db4e2427560031f3f9beec3ab
Alexandre Lafaye <lafa.alexandre@gmail.com>
2016-12-20 15:10:47 +0100

    fixes

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 
 unsigned int tick_led;
 
+// TODO Check if good
 //Transform factor between 91 and 255
 #define TRANSFORM_FACTOR 2.8
 // define to the pot val between 0-255
@@ -21,11 +22,13 @@ unsigned int tick_led;
 // lenght of the palette
 #define lenght 10
 
+// Function for basic A/D initialisation
 void init_AD(void){
-    P6SEL = 0x0F; // Enable A/D Channel inputs
-    //ADC12 is on + multiple sample and convert
+    // P6SEL = 0x0F; // Enable A/D Channel inputs
+    P6SEL = BIT5; // Enable A/D Channel inputs
+    //ADC12 is on + multiple sample and convert on 256 cycles
     ADC12CTL0 = ADC12ON+ADC12MSC+ADC12SHT0_8; 
-    ADC12CTL1 = ADC12SHP+ADC12CONSEQ_3;
+    ADC12CTL1 = ADC12SHP+ADC12CONSEQ_2;
     // Channel A5
     ADC12MCTL5 = ADC12INCH_5 + ADC12EOS; 
     // Enable encoder
@@ -34,6 +37,15 @@ void init_AD(void){
     ADC12CTL0 |= ADC12SC; 
 }
 
+// To ready the S1 button
+void init_S1(void){
+    P1DIR &= ~BIT7;          // Set P1.7 as input S1
+    P1REN |= BIT7;  // Enable P1.7 internal resistance
+    P1OUT |= BIT7;  // Set P1.7 as pull-Up resistance
+    P1IES |= BIT7;  // P1.7 Hi/Lo edge
+}
+
+// TODO test
 // To power the potentiometer
 void init_pot(void){
     P8DIR |= BIT0;
@@ -55,8 +67,10 @@ void init(void){
     init_screen();
     init_AD();
     init_pot();
+    init_S1(void);
 }
 
+// TODO remove at the end!
 // To make the Led blink following a particuliar pattern
 // Ticking 3 times at 200ms interval, then off for 3s
 void blink_led(void){
@@ -69,6 +83,7 @@ void blink_led(void){
     }
 }
 
+// TODO look if easy way to clean lines with negative draw
 // Clears the line and draw the palette position
 // Uses VAL_POT, TRANSFORM_FACTOR
 void draw_palette(void){
@@ -84,6 +99,14 @@ void draw_palette(void){
     Dogs102x6_horizontalLineDraw(pos_palette, pos_palette + lenght, row_2, 0);
 }
 
+// Check if the button is pressed and recall init if it is the case
+void reset_button(void){
+    //If the button is pressed recall the init func
+    if((P1IN & BIT7)==0){
+        init(); // NOT GOOD TODO
+    }
+}
+
 // Main function
 void main(void)
 {
@@ -95,8 +118,12 @@ void main(void)
     init();
 
     // Add all the tasks
+    // TODO remove blink_led & indicate blink lenght
     SCH_Add_Task(blink_led, 0, 10);
+    // TODO check timing
     SCH_Add_Task(draw_palette, 0, 10);
+    SCH_Add_Task(reset_button, 0, 10);
+    // TODO Add ball for the game
     // SCH_Add_Task(draw_ball, 0, 10);
 
     // Initialisation de l'ordonnanceur
